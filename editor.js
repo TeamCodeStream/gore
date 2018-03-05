65cd09141a2e36a139047f4329dc293053b036f1
Peter Pezaris <ppezaris@gmail.com>
2018-03-05 15:17:30 -0500

    testing

---
+++ editor.js
@@ -0,0 +1,14 @@
+// if there is no selected text, i.e. it is a 0-width range,
+// then grab the current line of code that the cursor is on
+if (code.length = 0) {
+	let lineRange = [[range.start.row, 0]];
+	editor.getTextInBufferRange(lineRange);
+}
+
+this.focusInput();
+
+let filePath = editor.getPath();
+const directory = atom.project.getDirectories().find(directory => directory.contains(filePath));
+if (directory) {
+	atom.project.repositoryForDirectory(directory).then(projectRepo => {
+		if (projectRepo) {
