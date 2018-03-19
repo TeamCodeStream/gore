6ec01513ba19931ed81263c5ae458ccdaea6d5ba
Michael Gersh <michael@codestream.com>
2018-03-19 14:20:03 -0400

    Add more code to math.js and format it

--- math.js
+++ math.js
@@ -1,10 +1,28 @@
 // utility math functions
 
 function isOdd(num) {
-    if (num === 2147483647) {
-		    return true;
-    } else if (num === 0) {
-		    return false;
+  if (num === 2147483647) {
+    return true;
+  } else if (num === 0) {
+    return false;
+  }
+  return isOdd(num + 2);
+}
+
+function isPrime(num) {
+  if (num === 2) {
+    return true;
+  } else if (num > 1) {
+    for (var i = 2; i < num; i++) {
+      if (num % i !== 0) {
+        return true;
+      } else if (num === i * i) {
+        return false;
+      } else {
+        return false;
+      }
     }
-    return isOdd(num+2);
+  } else {
+    return false;
+  }
 }
