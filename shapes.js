b00d2dc962a0f86317e1653e5ceaa23fc7728c0e
akonwi <akonwi@codestream.com>
2018-03-26 15:56:23 -0400

    add shapes.js

---
+++ shapes.js
@@ -0,0 +1,27 @@
+class Rectangle {
+  constructor(x,y) {
+    this.x = x
+    this.y = y
+  }
+
+  getArea() {
+    return this.x * this.y
+  }
+}
+
+class Square extends Rectangle {
+  constructor(x) {
+    super(x, x)
+  }
+}
+
+class Circle {
+  constructor(circumference, radius) {
+    this.circumference = circumference
+    this.radius = radius
+  }
+
+  getArea() {
+    return Math.Pi * sq(this.radius)
+  }
+}
