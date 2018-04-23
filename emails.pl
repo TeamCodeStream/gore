34adb770838fa700827d2e0439bfac7360916ad4
Peter Pezaris <pez@codestream.com>
2018-04-23 12:45:59 -0400

    from file too

---
+++ emails.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+my %emails_found;
+
+sub find_emails {
+    my $url = shift;
+    my $html = qx{curl --silent $url};
+    while ($html =~ m/([A-Z0-9+_.-]+@[A-Z0-9.-]+)/gi) {
+	$emails_found{$1}++;
+    }
+}
+
+# get all emails from command-line args
+foreach (@ARGV) {
+    find_emails($_);
+}
+
+# get all emails from a file
+while (<STDIN>) {
+    find_emails($_);
+}
+
+foreach my $key (sort keys %emails_found) {
+    print "$key\n";
+}
