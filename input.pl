414f455474094f052a36ae9132fc24967a76dd6e
Peter Pezaris <pez@codestream.com>
2018-08-28 13:45:15 -0400

    fm

---
+++ input.pl
@@ -0,0 +1,11 @@
+while (<STDIN>) {
+    process(trim($_));
+}
+
+sub process() {
+    my $url = shift;
+    my $html = qx{curl --silent $url};
+    while ($html =~ m/([A-Z0-9+_.-]+@[A-Z0-9.-]+)/gi) {
+	$emails_found{$1}++;
+    }
+}
