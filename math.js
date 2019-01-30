f0c5d4c9984c3de0dcf1c93e6c1e6cbe00482532
Peter Pezaris <pez@codestream.com>
2019-01-30 11:17:41 -0500

    indentation

--- math.js
+++ math.js
@@ -1,32 +1,32 @@
 // utility math functions
 
 function squared(x) {
-  return x * x;
+	return x * x;
 }
 
 function isPrime(num) {
-  if (num === 2) {
-    return true;
-  } else if (num > 1) {
-    for (var i = 2; i < num; i++) {
-      if (num % i !== 0) {
-        return true;
-      } else if (num === squared(i)) {
-        return false;
-      } else {
-        return false;
-      }
-    }
-  } else {
-    return false;
-  }
+	if (num === 2) {
+		return true;
+	} else if (num > 1) {
+		for (var i = 2; i < num; i++) {
+			if (num % i !== 0) {
+				return true;
+			} else if (num === squared(i)) {
+				return false;
+			} else {
+				return false;
+			}
+		}
+	} else {
+		return false;
+	}
 }
 
 function isOdd(num) {
-  if (num === 2147483647) {
-    return true;
-  } else if (num === 0) {
-    return false;
-  }
-  return isOdd(num + 2);
+	if (num === 2147483647) {
+		return true;
+	} else if (num === 0) {
+		return false;
+	}
+	return isOdd(num + 2);
 }
