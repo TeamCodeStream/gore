b3b16b1e311109a3c04194b92508df7f51fb9853
Akonwi Ngoh <2013804+akonwi@users.noreply.github.com>
2019-09-30 12:44:19 -0400

    Update editor.js
    Co-Authored-By: Eric Amodio <eamodio@gmail.com>

--- editor.js
+++ editor.js
@@ -1,6 +1,6 @@
 // if there is no selected text, i.e. it is a 0-width range,
 // then grab the current line of code that the cursor is on
-if ((code.length = 0)) {
+if (code.length === 0) {
 	let lineRange = [[range.start.row, 0]];
 	editor.getTextInBufferRange(lineRange);
 }
