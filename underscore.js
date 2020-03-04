360aad9478f5b7e1fee3bea01c349799db19b97a
Michael Gersh <michael@Michaels-Air-4.home>
2020-03-04 12:04:58 -0500

    Refactor sorting

--- underscore.js
+++ underscore.js
@@ -27,6 +27,29 @@ _.min = function(obj, iteratee, context) {
 	return result;
 };
 
+_.sortBy = function(obj, iteratee, context) {
+	var index = 0;
+	iteratee = cb(iteratee, context);
+	return _.pluck(
+		_.map(obj, function(value, key, list) {
+			return {
+				value: value,
+				index: index++,
+				criteria: iteratee(value, key, list)
+			};
+		}).sort(function(left, right) {
+			var a = left.criteria;
+			var b = right.criteria;
+			if (a !== b) {
+				if (a > b || a === void 0) return 1;
+				if (a < b || b === void 0) return -1;
+			}
+			return left.index - right.index;
+		}),
+		"value"
+	);
+};
+
 _.shuffle = function(obj) {
 	return _.sample(obj, Infinity);
 };
