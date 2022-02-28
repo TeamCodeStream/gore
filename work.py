3570c3f91df46c00c057b4663d825172be59c32e
James Price <jimmyjayp@gmail.com>
2022-02-28 14:26:35 -0500

    A script for working

---
+++ work.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Do some work')
+parser.add_argument('--debug', action='store_true', default=False, help='show debugging output')
+parser.add_argument('--it', action='store_true', default=False, help="work it")
+args = parser.parse_args()
+
+if not args.it:
+    parser.print_help()
+    exit(1)
+
+print("work work work....all I do is work.")
+exit(0)
