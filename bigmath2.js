dd25bddd783a5a30fa9a5168ab295e34b51703a8
Dillon Sellars <dsellars@newrelic.com>
2022-03-09 11:07:34 -0500

    Test CDSTRM-703

--- bigmath2.js
+++ bigmath2.js
@@ -15,7 +15,7 @@ function isPrime(num) {
 	if (num === 2) {
 		return true;
 	} else if (num > 1) {
-		for (var i = 2; i < num; i++) {
+		for (let i = 2; i < num; i++) {
 			if (num % i !== 0) {
 				return true;
 			} else if (num === squared(i)) {
