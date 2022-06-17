c460cf303743e33df534b442ff94740f14bb95f6
Marcelo Bukowski de Farias <mfarias@newrelic.com>
2022-06-17 15:34:29 -0400

    Over and over

--- input.pl
+++ input.pl
@@ -8,7 +8,7 @@ sub process() {
     // also changed this file
     my $html = qx{curl --silent $url};
     while ($html =~ m/([A-Z0-9+_.-]+@[A-Z0-9.-]+)/gi) {
-        // changed this file again one more time
+        // changed this file again one more time, over and over
 	    $emails_found{$1}++;
     }
 }
