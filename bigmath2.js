4bca23743f4295a7e5d984b07ccd5a446d6118dd
Brett Paden <brettpaden@gmail.com>
2022-11-09 12:28:06 -0500

    Removed one of the many isPrime functions just to play around (#89)
    Co-authored-by: Brett Paden <bpaden@newrelic.com>

--- bigmath2.js
+++ bigmath2.js
@@ -333,23 +333,6 @@ function superNewStuff() {
     return 1 + 1;
 }
 
-function isPrime(num) {
-	if (num === 2) {
-		return true;
-	} else if (num > 1) {
-		for (var i = 2; i < num; i++) {
-			if (num % i !== 0) {
-				return true;
-			} else if (num === squared(i)) {
-				return false;
-			} else {
-				return false;
-			}
-		}
-	} else {
-		return false;
-	}
-}
 
 function someBrandNewStuff() {
     return 1 + 1;
